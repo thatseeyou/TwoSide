33b6ee5c24251cdc542b384ee780a593fa5f5166
Sang Young Oh <sangyoung@gmail.com>
2015-11-09 14:36:31 +0900

    내용 변경 없음

--- TwoSide/AppDelegate.swift
+++ TwoSide/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    var count = 1
+    var count  = 1
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
